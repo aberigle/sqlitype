9fd701ccf4b198919680ff091138b8148b938612
aberigle <aberigle@uhurus.com>
2025-10-14 12:04:02 +0200

    add where clauses for comparisons

--- src/core/queries/build-where.ts
+++ src/core/queries/build-where.ts
@@ -1,19 +1,35 @@
 import { Field } from "../field";
 import { getFieldName } from "../field/serialize";
 
+function getActionFromValue(value: any) {
+  if (
+    typeof value == 'string' && value.includes("%")
+  ) return { action: "LIKE", value }
+
+  if (
+    typeof value !== 'object' ||
+    value?.getTime // Date
+  ) return { action: "=", value }
+
+  if (value.$lt)  return { action: "<",  value: value.$lt }
+  if (value.$lte) return { action: "<=", value: value.$lte }
+  if (value.$gte) return { action: ">=", value: value.$gte }
+  if (value.$gt)  return { action : ">", value: value.$gt }
+}
+
 export function buildWhere(
   fields: Record<string, Field>,
-  filter : Record<string, any>,
+  filter: Record<string, any>,
   table: string = ""
-) : {
-    sql: string, args: any[], joins : Record<string, Field>
+): {
+  sql: string, args: any[], joins: Record<string, Field>
 } {
   const keys = Object.keys(filter)
 
   if (!keys.length) return { sql: '', args: [], joins: {} }
 
-  const values     : any[] = []
-  const conditions : string[] = []
+  const values: any[] = []
+  const conditions: string[] = []
   const joins: Record<string, Field> = {}
 
   for (const name of keys) if (fields[name]) {
@@ -23,19 +39,15 @@ export function buildWhere(
       continue
     }
 
-    let action = "="
-    const value = filter[name]
-
-    if (typeof value == 'string' && value.includes("%"))
-      action = "LIKE"
+    const { action, value } = getActionFromValue(filter[name])
 
     values.push(field.cast(value))
     conditions.push(`${table ? table + "." : ''}"${getFieldName(name, field)}" ${action} ?`)
   }
 
   return {
-    sql  : `${conditions.join(" AND ")}`,
-    args : values,
+    sql: `${conditions.join(" AND ")}`,
+    args: values,
     joins
   }
 }
